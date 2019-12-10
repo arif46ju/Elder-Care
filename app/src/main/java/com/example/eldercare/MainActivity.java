d8cccb49442f7e6a8da0babc8d4ffa485b3aa89c
kohinurparvin11@gmail.com <kohinurparvin11@gmail.com>
2019-12-10 12:39:10 +0600

    navigation activity

--- app/src/main/java/com/example/eldercare/MainActivity.java
+++ app/src/main/java/com/example/eldercare/MainActivity.java
@@ -7,83 +7,48 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button button1, button2, button3, button4,button5,button6, button7, button8, button9;
+    private Button button1, button2, button3, button4, button5, button6, button7, button8, button9;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1=(Button) findViewById(R.id.button1);
-        button2=(Button) findViewById(R.id.button2);
-        button3=(Button) findViewById(R.id.button3);
-        button4=(Button) findViewById(R.id.button4);
-        button5=(Button) findViewById(R.id.button5);
-        button6=(Button) findViewById(R.id.button6);
-        button7=(Button) findViewById(R.id.button7);
-        button8=(Button) findViewById(R.id.button8);
-        button9=(Button) findViewById(R.id.button9);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+    }
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openDailyActivities();
+        @Override
+        public void onClick (View v){
+            if (v.getId() == R.id.button1) {
+                Intent intent = new Intent(this, DailyActivitiesActivity.class);
+                startActivity(intent);
             }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openMedicationsActivity();
+            if (v.getId() == R.id.button2) {
+                Intent intent = new Intent(this, MedicationsActivity.class);
+                startActivity(intent);
             }
-        });
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openGroceryActivity();
+            if (v.getId() == R.id.button3) {
+                Intent intent = new Intent(this, GroceryActivity.class);
+                startActivity(intent);
             }
-        });
-
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openOnlineShoppingActivity();
+            if (v.getId() == R.id.button4) {
+                Intent intent = new Intent(this, OnlineShoppingActivity.class);
+                startActivity(intent);
             }
-        });
-
-
-
-
-
-
-
-    }
-
-    public void openDailyActivities(){
-        Intent intent=new Intent(this, DailyActivitiesActivity.class);
-        startActivity(intent);
-    }
-
-    public void openMedicationsActivity(){
-        Intent intent=new Intent(this, MedicationsActivity.class);
-        startActivity(intent);
-    }
-
-    public void openGroceryActivity(){
-        Intent intent=new Intent(this, GroceryActivity.class);
-        startActivity(intent);
-    }
-
-    public void openOnlineShoppingActivity(){
-        Intent intent=new Intent(this, OnlineShoppingActivity.class);
-        startActivity(intent);
-    }
-
-
-
-
+        }
 
 }
